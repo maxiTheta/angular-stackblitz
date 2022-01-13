295a89ae698445b35d817da55eda22cff235cc82
maxiTheta <mhoeebs@posteo.de>
2022-01-13 16:09:54 +0100

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { CartService } from '../cat.service';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
