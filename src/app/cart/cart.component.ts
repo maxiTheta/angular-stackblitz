2bf504bc6a62652010eace7825a077941090b10c
maxiTheta <mhoeebs@posteo.de>
2022-01-13 16:06:21 +0100

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cat.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  item = this.cartService.getItems();
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
   }
