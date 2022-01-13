fa0a5aeb5ed761f12e7530c7810085d800bfda88
maxiTheta <mhoeebs@posteo.de>
2022-01-13 16:26:02 +0100

    3 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClien } from '@angular/common/http';
 
 import { Product } from './products';
 
@@ -8,7 +9,9 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -21,5 +24,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
