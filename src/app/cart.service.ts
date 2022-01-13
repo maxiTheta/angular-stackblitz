4c92cd4287336e6eb030761ba7c60f04949c1524
maxiTheta <mhoeebs@posteo.de>
2022-01-13 16:29:25 +0100

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClien } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { Product } from './products';
 
